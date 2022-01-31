e3e05a83f7a91761c6c50ccc8dc7d8ed765084d6
jacobuhlig@gmail.com <birth_chubby_GLEN_thrips>
2022-01-31 09:43:34 +0100

    ArrayList challenge done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
+class App {
+
+    public void run() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("hey");
+        arrayList.add("boi");
+        arrayList.add("hello");
+        //arrayList.add("string");
+
+        String string = "string";
+        boolean booleanValue = doesArrayListContainString(arrayList, string);
+        System.out.println(booleanValue);
+    }
+
+    public boolean doesArrayListContainString(ArrayList<String> arrayList, String string) {
+            if (arrayList.contains(string)) {
+                System.out.println("The String has been found");
+                return true;
+            }
+    return false;
+    }
+}
+
 public class Main {
+
+    public static void main(String[] args) {
+            new App().run();
+    }
 }
